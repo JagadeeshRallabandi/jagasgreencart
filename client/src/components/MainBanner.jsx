1f4125be2e066ef2443d60aab1ff41a5b8f1ba95
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-19 16:17:44 +0530

    Main_Banner_issue_solved

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const MainBanner = () => {
   return (
-    <div className='relative mt-10'>
+    <div className='relative mt-10 z-0'>
         <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block'/>
         <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden'/>
         <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
