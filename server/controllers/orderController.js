2d0cbf972b83e4112a090f3607c4d29326e54f6b
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-06-19 13:13:18 +0530

    get_All_Orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -170,7 +170,11 @@ export const getUserOrders = async (req,res) => {
         const userId = req.userId;
         const orders = await Order.find({
             userId,
-            $or : [{paymentType:"COD"}, {isPaid:true}]
+            $or: [
+  { paymentType: { $in: ["COD", "Online"] } },
+  { isPaid: true }
+]
+
         }).populate("items.product address").sort({createdAt:-1});
         res.json({success:true, orders});
     } catch (error) {
@@ -183,7 +187,11 @@ export const getAllOrders = async (req,res) => {
     try {
 
         const orders = await Order.find({
-            $or : [{paymentType:"COD"}, {isPaid:true}]
+            $or: [
+  { paymentType: { $in: ["COD", "Online"] } },
+  { isPaid: true }
+]
+
         }).populate("items.product address").sort({createdAt:-1});
         res.json({success:true, orders});
     } catch (error) {
